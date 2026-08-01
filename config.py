3487cb33f78cda574f15e636fba7c647aff2b377
tamnv1308-ship-it <tamnv1308@gmail.com>
2026-08-01 08:42:05 +0700

    config.py

--- config.py
+++ config.py
@@ -9,3 +9,11 @@ GOOGLE_SERVICE_ACCOUNT = os.getenv("GOOGLE_SERVICE_ACCOUNT_JSON", "")
 BOT_PREFIX = "!"
 CACHE_TIME = 300
 BOT_NAME = "LINE FAQ BOT"
+REMINDER_GROUP_ID = "Ca6ebad8571ec436ed0cc4a68729d22c0"
+
+REMINDER_TEXT = """
+Chào buổi sáng mọi người.
+
+Chúc cả group một ngày thật nhiều năng lượng và làm việc hiệu quả.
+Có gì cần tra cứu, mọi người cứ gọi em nhé.
+""".strip()
