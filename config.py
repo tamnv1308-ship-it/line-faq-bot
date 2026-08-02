89b22e881e036f8badd3677de3531880a8258592
tamnv1308-ship-it <tamnv1308@gmail.com>
2026-08-02 21:51:17 +0700

    config.py

--- config.py
+++ config.py
@@ -9,14 +9,45 @@ GOOGLE_SERVICE_ACCOUNT = os.getenv("GOOGLE_SERVICE_ACCOUNT_JSON", "")
 BOT_PREFIX = "!"
 CACHE_TIME = 300
 BOT_NAME = "LINE FAQ BOT"
-REMINDER_GROUP_ID = "Ca6ebad8571ec436ed0cc4a68729d22c0"
-
-REMINDER_TEXT = """
-Chào buổi sáng mọi người.
-
-Chúc cả group một ngày thật nhiều năng lượng và làm việc hiệu quả.
-Có gì cần tra cứu, mọi người cứ gọi em nhé.
-""".strip()
+REMINDERS = [
+    {
+        "group_id": "Ca6ebad8571ec436ed0cc4a68729d22c0",
+        "message": "Chào buổi sáng Group 1.\nChúc mọi người một ngày hiệu quả.",
+    },
+    {
+        "group_id": "Ca6ebad8571ec436ed0cc4a68729d22c0",
+        "message": "Chào buổi sáng Group 2.\nNội dung riêng của group 2.",
+    },
+    {
+        "group_id": "Ca6ebad8571ec436ed0cc4a68729d22c0",
+        "message": "Nội dung riêng của group 3.",
+    },
+    {
+        "group_id": "Ca6ebad8571ec436ed0cc4a68729d22c0",
+        "message": "Nội dung riêng của group 4.",
+    },
+    
+    {
+        "group_id": "Ca6ebad8571ec436ed0cc4a68729d22c0",
+        "message": "Nội dung riêng của group 5.",
+    },
+    
+    {
+        "group_id": "Ca6ebad8571ec436ed0cc4a68729d22c0",
+        "message": "Nội dung riêng của group 6.",
+    },
+    
+    {
+        "group_id": "Ca6ebad8571ec436ed0cc4a68729d22c0",
+        "message": "Nội dung riêng của group 7.",
+    },
+    
+    {
+        "group_id": "Ca6ebad8571ec436ed0cc4a68729d22c0",
+        "message": "Nội dung riêng của group 7.",
+    },
+        
+]
 
 ADMIN_USER_IDS = [
     "U60751d1a57eb4707a3dff9c06f3240a4"
