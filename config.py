6457f81ee440807233ce871f0dfcce4ba1746c37
tamnv1308-ship-it <tamnv1308@gmail.com>
2026-08-05 22:17:11 +0700

    config.py

--- config.py
+++ config.py
@@ -9,45 +9,7 @@ GOOGLE_SERVICE_ACCOUNT = os.getenv("GOOGLE_SERVICE_ACCOUNT_JSON", "")
 BOT_PREFIX = "!"
 CACHE_TIME = 300
 BOT_NAME = "LINE FAQ BOT"
-REMINDERS = [
-    {
-        "group_id": "Ca6ebad8571ec436ed0cc4a68729d22c0",
-        "message": "Chào buổi sáng Group 1.\nChúc mọi người một ngày hiệu quả.",
-    },
-    {
-        "group_id": "C4f38e1a465a6dd1b0a3cd5c175f84c62",
-        "message": "Chào buổi sáng Group 2.\nNội dung riêng của group 2.",
-    },
-    {
-        "group_id": "C4f38e1a465a6dd1b0a3cd5c175f84c62",
-        "message": "Nội dung riêng của group 3.",
-    },
-    {
-        "group_id": "C4f38e1a465a6dd1b0a3cd5c175f84c62",
-        "message": "Nội dung riêng của group 4.",
-    },
-    
-    {
-        "group_id": "C4f38e1a465a6dd1b0a3cd5c175f84c62",
-        "message": "Nội dung riêng của group 5.",
-    },
-    
-    {
-        "group_id": "C4f38e1a465a6dd1b0a3cd5c175f84c62",
-        "message": "Nội dung riêng của group 6.",
-    },
-    
-    {
-        "group_id": "C4f38e1a465a6dd1b0a3cd5c175f84c62",
-        "message": "Nội dung riêng của group 7.",
-    },
-    
-    {
-        "group_id": "C4f38e1a465a6dd1b0a3cd5c175f84c62",
-        "message": "Nội dung riêng của group 8.",
-    },
-        
-]
+
 GROUPS = {
     "hn": "Ca6ebad8571ec436ed0cc4a68729d22c0",
     "hcm": "C4f38e1a465a6dd1b0a3cd5c175f84c62",
@@ -58,4 +20,29 @@ ADMIN_USER_IDS = [
     "Ud29d838c51efaecd363ff5bc9ffa2a4c",
     "U810ef43dc32d6cb1c3bb332f7aabad4c",
     "Ua7faef12dfde3213c23d01c34dfede91",
+]
+
+REMINDER_SCHEDULES = [
+    {
+        "id": "morning",
+        "hour": 8,
+        "minute": 0,
+        "reminders": [
+            {
+                "group_id": "Ca6ebad8571ec436ed0cc4a68729d22c0",
+                "message": "☀️ Chào buổi sáng mọi người, hẹ hẹ hẹ.\nChúc cả group một ngày chạy deadline té địt nhớ !!!.",
+            },
+        ],
+    },
+    {
+        "id": "night",
+        "hour": 22,
+        "minute": 0,
+        "reminders": [
+            {
+                "group_id": "Ca6ebad8571ec436ed0cc4a68729d22c0",
+                "message": "🌙 Con đỗn Boa, thoát nhóm ra vào lại nhá nhanh lên nàoo!.",
+            },
+        ],
+    },
 ]
